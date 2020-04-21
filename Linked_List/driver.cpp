b1069a360cd97a6a580a8821195fbd48f9df18c0
TobyDS <tobydixonsmith@gmail.com>
2020-04-20 18:12:13 -0600

    Update driver.cpp

--- Linked_List/driver.cpp
+++ Linked_List/driver.cpp
@@ -1,5 +1,5 @@
 /*H**********************************************************************
-* FILENAME :        ll.cpp
+* FILENAME :        driver.cpp
 *
 * DESCRIPTION :
 *       File tests linked list class
@@ -11,6 +11,8 @@
 *H*/
 
 #include <iostream>
+#include <chrono>
+#include <iomanip>
 #include "ll.hpp"
 
 using namespace std;
@@ -19,9 +21,17 @@ int main()
 {
     LinkedList l;
     l.printLL();
+
+    auto start = chrono::high_resolution_clock::now();
     l.insertNode(NULL,39821);
     l.insertNode(l.searchLL(39821), 143591);
     l.insertNode(l.searchLL(143591), 57518);
+    auto end = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+    time_taken *= 1e-9;
+
+    cout << "Insertion time: " << time_taken << fixed << setprecision(9) << " sec" << endl;
+
     l.printLL();
     l.searchLL(1);
 
