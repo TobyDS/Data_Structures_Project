c17a5b954894dec396989fa0193c4064dafb0a1f
TobyDS <tobydixonsmith@gmail.com>
2020-04-21 13:30:01 -0600

    Update driver.cpp
    Insertion and timing working

--- Linked_List/driver.cpp
+++ Linked_List/driver.cpp
@@ -66,13 +66,33 @@ void readData(int testData[], string dataPath)
     }
 }
 
+/* 
+ *  Function:  insertIntoLL
+ * --------------------
+ *  Inserts data from an array into a linked list
+ * 
+ *  l: a linked list to be inserted into
+ *  insertData: an array of ints to be inserted into the linked list
+ *  startIndex: the index in the array to start inserting at
+ *  numToInsert: the number of elements to insert into the linked list
+ * 
+ *  returns: nothing
+*/
+void insertIntoLL(LinkedList &l, int insertData[], int startIndex, int numToInsert)
+{
+    for (int i = startIndex; i < startIndex + numToInsert; i++) // Insert numbers form start index to end index
+    {
+        l.insertNode(NULL, insertData[i]); // Insert all node as the start of the linked list.
+    }
+}
+
 int main(int argc, char *argv[])
 {
     LinkedList l;
     int MAX_ELEMENTS = 40000;   // There are 40,000 data entries in CSVs
     int testData[MAX_ELEMENTS]; // Contains all test data
-    double insert[400];         // Contains times taken to insert 100 elements
-    double search[400];         // Contains times taken to search elements
+    double insert[400];         // Contains average time taken to insert an element (seconds)
+    double search[400];         // Contains times taken to search elements (seconds)
 
     if (argc == 2) // Correct number of command line args
     {
@@ -89,6 +109,22 @@ int main(int argc, char *argv[])
                 dataPath = "../Data/dataSetB.csv";
             }
             readData(testData, dataPath);
+            // INSERTION INTO LL AND TIMING CALCULATION
+            for (int numEntries = 0; numEntries < MAX_ELEMENTS; numEntries += 100)
+            {
+                auto start = chrono::high_resolution_clock::now();                                  // Start clock
+                insertIntoLL(l, testData, numEntries, 100);                                         // Insert 100 entries
+                auto end = chrono::high_resolution_clock::now();                                    // Stop the clock
+                double timeTaken = chrono::duration_cast<chrono::nanoseconds>(end - start).count(); // Calculate time taken in nano seconds
+                timeTaken *= 1e-9;                                                                  // Convert from nano seconds into seconds
+                double averageTimeTaken = timeTaken / 100;                                          // Find average entry time
+                insert[numEntries / 100] = averageTimeTaken;                                        // Log time taken for average entry
+                
+                if(numEntries % 4000 == 0) // Print a 10 entries as test
+                {
+                    cout << "Average insertion time: " << averageTimeTaken*1e9 << setprecision(4) << " nanosec" << endl;
+                }
+            }
         }
         else // Incorrect name for data set
         {
@@ -99,7 +135,7 @@ int main(int argc, char *argv[])
     }
     else // Incorrect number of command line args
     {
-        cerr << "Error: not enough args. Please refer to usage" << endl;
+        cerr << "Error: incorrect number of args. Please refer to usage" << endl;
         cout << endl;
         printUsage();
     }
