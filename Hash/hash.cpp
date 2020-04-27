c2c676c7115feef69b8e637581add24199a5bcf6
Vinipeli <vipe8838@colorado.edu>
2020-04-26 22:52:12 -0300

    Implemented insert functions/One search fucntion

---
+++ Hash/hash.cpp
@@ -0,0 +1,21 @@
+/*H**********************************************************************
+* FILENAME :        hash.cpp
+*
+* DESCRIPTION :
+*       File contains implementation for Hash Table class
+*
+* NOTES :
+* 
+* AUTHORS : Toby Dixon Smith & Vinicius Pelissari     START DATE : 26 Apr 20
+*
+*H*/
+
+#include <iostream>
+#include "hash.hpp"
+
+//Class Functions
+
+HashTable::HashTable(int bsize)
+{
+    node* 
+}
